fdf2c277ecd44f8f806f0386398ce5748c3ae39d
jamie082 <42884063+jamie082@users.noreply.github.com>
2023-08-23 06:57:34 +0000

    added while (true) statement

--- for_java.java
+++ for_java.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+/* Menu driven program. Enter 1 for convert inch to meters. Enter 2 to convert gallons to liters. Loop if one or 2 wasnt successful */
+
 public class Main {
   public static void main(String[] args) {
 
@@ -7,17 +9,30 @@ public class Main {
   //boolean set_statement = false;
     // start program
     // where is the while(!set_statement) {
+    double gallons;
+    double liters;
 
-    switch (my_input) {
-      case 1: inputString = "Input a value for inch: ";
-        break;
-      case 2: inputString = "Input a value for gallon: "
-    }
+   // liters = gallons * 3.7854; // convert to liters
+
+    /*
+
+    System.out.println("Welcome to my Conversion Program by Jamie v.01")
+    System.out.println("Please select 1 for inch conversion or 2 for gallon conversion")
+    while (true) {
+      switch (my_input) {
+        case 1: inputString = "1.) Input a value for inch: ";
+          break;
+        case 2: inputString = "2.) Input a value for gallon: ";
+          break;
+        }
+      }
+    */
     try {
       System.out.print("Input a value for inch: ");
       double inch = input.nextDouble();
       double meters = inch * 0.0254;
       System.out.println(inch + " inch is " + meters + " meters");
+      System.out.println(gallons + " gallons is " + liters + " liters.");
   } catch(Exception e) {
       System.out.println("Not a valid number.");
       // Clear out input buffer
